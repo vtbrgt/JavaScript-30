e264a1d072348e12f1995598b98cd47fba8467d7
Vitor Borgatte <79037838+vtbrgt@users.noreply.github.com>
2022-07-18 12:46:03 -0300

    starting new challenge

---
+++ array-cardio-day-2/main.js
@@ -0,0 +1,39 @@
+// ## Array Cardio Day 2
+
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 },
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 },
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const nineteen = people.some((person) => {
+  return 2022 - person.year >= 19;
+});
+
+/* console.log(nineteen); */
+
+// Array.prototype.every() // is everyone 19 or older?
+const everyoneNineteen = people.every((person) => {
+  return 2022 - person.year >= 19;
+});
+
+console.log(everyoneNineteen);
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
