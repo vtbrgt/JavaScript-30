9ed701ee0d379a725c4d87b845d34d8c6c44bdd4
Vitor Borgatte <79037838+vtbrgt@users.noreply.github.com>
2022-07-26 11:20:32 -0300

    finished challenge

--- array-cardio-day-2/main.js
+++ array-cardio-day-2/main.js
@@ -28,12 +28,24 @@ const everyoneNineteen = people.every((person) => {
   return 2022 - person.year >= 19;
 });
 
-console.log(everyoneNineteen);
+/* console.log(everyoneNineteen); */
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const chosenID = comments.find((comment) => {
+  return comment.id === 823423;
+});
+
+/* console.log(chosenID); */
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const index = comments.findIndex((comment) => {
+  return comment.id === 823423;
+});
+
+comments.splice(index, 1);
+
+console.log(comments);
